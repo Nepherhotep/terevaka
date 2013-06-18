52864ad1450eb09e7d411153ca5fdc4f9b6e04da
Alexey Zankevich <zancudero@gmail.com>
2013-06-18 16:49:07 +0300

    updated prop util

--- TKPropUtil.lua
+++ TKPropUtil.lua
@@ -1,6 +1,6 @@
 module(..., package.seeall)
 
-function flipProp(prop, flipped)
+function flopProp(prop, flipped)
    local x, y = prop:getScl()
    if flipped then
       if x > 0 then
@@ -9,6 +9,20 @@ function flipProp(prop, flipped)
    else
       if x < 0 then
 	 x =-x 
+      end
+   end
+   prop:setScl(x, y)
+end
+
+function flipProp(prop, flopped)
+   local x, y = prop:getScl()
+   if flopped then
+      if y > 0 then
+	 y = -y
+      end
+   else
+      if y < 0 then
+	 y =-y 
       end
    end
    prop:setScl(x, y)
