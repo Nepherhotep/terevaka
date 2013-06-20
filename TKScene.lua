77a73cf65a61c3a9c3b9a65d046cae8507a09973
Alexey Zankevich <zancudero@gmail.com>
2013-06-20 13:01:47 +0300

    updated TKScene for MOAI code style

--- TKScene.lua
+++ TKScene.lua
@@ -1,44 +1,44 @@
 module(..., package.seeall)
 
-local TKResourceManager = require('terevaka/TKResourceManager')
-local TKScreen = require('terevaka/TKScreen')
+local TKResourceManager = require ( 'terevaka/TKResourceManager' )
+local TKScreen = require ( 'terevaka/TKScreen' )
 
 
 TKScene = {}
 
-function TKScene:new (o)
+function TKScene:new ( o )
    o = o or {}
-   setmetatable(o, self)
+   setmetatable ( o, self )
    self.__index = self
    o.viewCache = nil
    return o
 end
 
-function TKScene:fillLayer(params)
+function TKScene:fillLayer ( params )
    -- Unpack options
-   self:updateViewCacheTable(params.resourceName)
-   local resourceFile = TKResourceManager.findLayoutFile(params.resourceName)
+   self:updateViewCacheTable ( params.resourceName )
+   local resourceFile = TKResourceManager.findLayoutFile ( params.resourceName )
    params.resource = dofile ( resourceFile )
-   self:fillScalableLayout(params)
+   self:fillScalableLayout ( params )
 end
 
-function TKScene:fillScalableLayout(params)
+function TKScene:fillScalableLayout ( params )
    if params.texturePack then
       params.deck = params.texturePack.quads
       params.resourceScaleFactor = params.texturePack.resourceScaleFactor
    end
    params.layout_width = params.resource.layout_width
    params.layout_height = params.resource.layout_height
-   for i, propTable in ipairs(params.resource.props) do
+   for i, propTable in ipairs ( params.resource.props ) do
       if params.texturePack then
 	 params.index = params.texturePack.spriteNames[propTable.name]
       end
       params.propTable = propTable
-      self:addScalableProp(params)
+      self:addScalableProp ( params )
    end
 end
 
-function TKScene:addScalableProp(params)
+function TKScene:addScalableProp ( params )
    -- extract params
    local propTable = params.propTable
    local layout_height = params.layout_height
@@ -50,36 +50,36 @@ function TKScene:addScalableProp(params)
    -- do method
    local scaleFactor = TKScreen.SCREEN_HEIGHT / layout_height
    if propTable.h_align == 'center' then
-      horizontalOffset = (TKScreen.SCREEN_WIDTH - scaleFactor * layout_width)/2
+      horizontalOffset = ( TKScreen.SCREEN_WIDTH - scaleFactor * layout_width ) / 2
    else
       if propTable.h_align == 'left' then
 	 horizontalOffset = 0
       else
 	 if propTable.h_align == 'right' then
-	    horizontalOffset = (TKScreen.SCREEN_WIDTH - scaleFactor * layout_width)
+	    horizontalOffset = ( TKScreen.SCREEN_WIDTH - scaleFactor * layout_width )
 	 else
-	    horizontalOffset = propTable.x * (TKScreen.SCREEN_WIDTH / layout_width - scaleFactor)
+	    horizontalOffset = propTable.x * ( TKScreen.SCREEN_WIDTH / layout_width - scaleFactor )
 	 end
       end
    end
 
    local prop = MOAIProp2D.new ()
 
-   prop:setDeck(deck)
-   prop:setIndex(index)
+   prop:setDeck ( deck )
+   prop:setIndex ( index )
    x = horizontalOffset + scaleFactor * propTable.x
    y = scaleFactor * propTable.y
    prop:setLoc ( x, y )
-   prop:setScl( resourceScaleFactor )
+   prop:setScl ( resourceScaleFactor )
    if propTable.z_index then
-      prop:setPriority( propTable.z_index )
+      prop:setPriority ( propTable.z_index )
    end
-   params.layer:insertProp( prop )
-   self:cacheView( params.resourceName, propTable.uid, prop )
+   params.layer:insertProp ( prop )
+   self:cacheView ( params.resourceName, propTable.uid, prop )
    return prop
 end
 
-function TKScene:updateViewCacheTable(resourceName)
+function TKScene:updateViewCacheTable ( resourceName )
    -- Create cache table if it doesn't exist
    if self.viewCache == nil then
       self.viewCache = {}
@@ -89,13 +89,13 @@ function TKScene:updateViewCacheTable(resourceName)
    end
 end
 
-function TKScene:cacheView(resourceName, viewId, view)
+function TKScene:cacheView ( resourceName, viewId, view )
    if viewId ~= "" and viewId ~= nil then
       self.viewCache[resourceName][viewId] = view
    end
 end
 
-function TKScene:findPropById(layerResourceName, viewId)
+function TKScene:findPropById ( layerResourceName, viewId )
    if self.viewCache ~= nil then
       if self.viewCache[layerResourceName] ~= nil then
 	 return self.viewCache[layerResourceName][viewId]
@@ -104,37 +104,37 @@ function TKScene:findPropById(layerResourceName, viewId)
    return nil
 end
 
-function TKScene:handleTouch(layer, event)
-   local prop = layer:getPartition():propForPoint(layer:wndToWorld(event.wndX, event.wndY))
+function TKScene:handleTouch ( layer, event )
+   local prop = layer:getPartition () :propForPoint ( layer:wndToWorld ( event.wndX, event.wndY ))
    if prop then
       if prop.onTouch then
-	 return prop:onTouch(event)
+	 return prop:onTouch ( event )
       end
    end
 end
 
-function TKScene:clear()
-   for i, layer in ipairs(self:getLayers()) do
-      layer:clear()
+function TKScene:clear ()
+   for i, layer in ipairs ( self:getLayers ()) do
+      layer:clear ()
    end
 end
 
 -- protocol for Terevaka.TKApplication
-function TKScene:getRenderTable()
-   print('Override TKScene:getLayers()')
+function TKScene:getRenderTable ()
+   print ( 'Override TKScene:getLayers ()' )
    return {}
 end
 
-function TKScene:onLoadScene()
-   print('Override TKScene:onLoadScene()')
+function TKScene:onLoadScene ()
+   print ( 'Override TKScene:onLoadScene ()' )
 end
 
-function TKScene:onRemoveScene()
-   print('Override TKScene:onRemoveScene()')
+function TKScene:onRemoveScene ()
+   print ( 'Override TKScene:onRemoveScene ()' )
 end
 
-function TKScene:onTouch(event)
-   print('Override TKScene:onTouch(event) method')
+function TKScene:onTouch ( event )
+   print ( 'Override TKScene:onTouch ( event ) method' )
 end
 
 return TKScene
