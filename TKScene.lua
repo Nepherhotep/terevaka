0a41b98411c4115b23ec41ab812ee2b112f59427
Alexey Zankevich <zancudero@gmail.com>
2013-07-31 18:24:42 +0300

    reformatted tkscene

--- TKScene.lua
+++ TKScene.lua
@@ -7,42 +7,42 @@ local TKScreen = require ( 'terevaka/TKScreen' )
 TKScene = {}
 
 function TKScene:new ( o )
-   o = o or {}
-   setmetatable ( o, self )
-   self.__index = self
-   o.viewCache = nil
-   return o
+    o = o or {}
+    setmetatable ( o, self )
+    self.__index = self
+    o.viewCache = nil
+    return o
 end
 
 function TKScene:init ()
-   return self
+    return self
 end
 
 function TKScene:handleTouch ( layer, event )
-   local prop = layer:getPartition () :propForPoint ( layer:wndToWorld ( event.wndX, event.wndY ))
-   if prop then
-      if prop.onTouch then
-	 return prop:onTouch ( event )
-      end
-   end
+    local prop = layer:getPartition () :propForPoint ( layer:wndToWorld ( event.wndX, event.wndY ))
+    if prop then
+        if prop.onTouch then
+            return prop:onTouch ( event )
+        end
+    end
 end
 
 -- protocol for Terevaka.TKApplication
 function TKScene:getRenderTable ()
-   print ( 'Override TKScene:getRenderTable ()' )
-   return {}
+    print ( 'Override TKScene:getRenderTable ()' )
+    return {}
 end
 
 function TKScene:onLoadScene ()
-   print ( 'Override TKScene:onLoadScene ()' )
+    print ( 'Override TKScene:onLoadScene ()' )
 end
 
 function TKScene:onRemoveScene ()
-   print ( 'Override TKScene:onRemoveScene ()' )
+    print ( 'Override TKScene:onRemoveScene ()' )
 end
 
 function TKScene:onTouch ( event )
-   print ( 'Override TKScene:onTouch ( event ) method' )
+    print ( 'Override TKScene:onTouch ( event ) method' )
 end
 
 return TKScene
