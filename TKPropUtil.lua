2e2b7f7d178d24f9b01fc4afbd67a3c5b742d4e1
Alexey Zankevich <zancudero@gmail.com>
2013-09-10 15:17:25 +0300

    Added new util - replaceProp

--- TKPropUtil.lua
+++ TKPropUtil.lua
@@ -35,3 +35,28 @@ function scaleProp ( prop, scaleFactor, scaleFactorY )
    end
    prop:setScl ( x * scaleFactor, y * scaleFactorY )
 end
+
+function replaceProp ( layer, propId, frame, texturePack )
+    local prop = layer:findPropById( propId )
+    local scaleX, scaleY = prop:getScl ()
+    local x, y = prop:getLoc()
+    local priority = prop:getPriority ()
+
+    local frameInfo = texturePack:getFrameInfo ( frame )
+    local index = frameInfo.index
+    local deck = frameInfo.deck
+    local resourceScaleFactor = frameInfo.resourceScaleFactor
+
+    local newProp = MOAIProp2D.new ()
+    newProp:setDeck ( deck )
+    newProp:setIndex ( index )
+
+    newProp:setLoc ( x, y )
+    newProp:setScl ( resourceScaleFactor )
+    newProp:setPriority ( priority )
+    newProp:setScl ( scaleX, scaleY )
+
+    layer:removeProp ( prop )
+    layer:insertProp ( newProp )
+    layer:cacheView ( propId, newProp )
+end
