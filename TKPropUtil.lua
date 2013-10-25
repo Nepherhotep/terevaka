9cd32a42accb9a66bc66d638ba5b1055e71e3116
Alexey Zankevich <zancudero@gmail.com>
2013-10-25 19:57:31 +0300

    updated idents

--- TKPropUtil.lua
+++ TKPropUtil.lua
@@ -1,39 +1,39 @@
 module(..., package.seeall)
 
 function flopProp ( prop, flipped )
-   local x, y = prop:getScl ()
-   if flipped then
-      if x > 0 then
-	 x = -x
-      end
-   else
-      if x < 0 then
-	 x =-x 
-      end
-   end
-   prop:setScl ( x, y )
+    local x, y = prop:getScl ()
+    if flipped then
+        if x > 0 then
+            x = -x
+        end
+    else
+        if x < 0 then
+            x =-x
+        end
+    end
+    prop:setScl ( x, y )
 end
 
 function flipProp ( prop, flopped )
-   local x, y = prop:getScl ()
-   if flopped then
-      if y > 0 then
-	 y = -y
-      end
-   else
-      if y < 0 then
-	 y =-y 
-      end
-   end
-   prop:setScl ( x, y )
+    local x, y = prop:getScl ()
+    if flopped then
+        if y > 0 then
+            y = -y
+        end
+    else
+        if y < 0 then
+            y =-y
+        end
+    end
+    prop:setScl ( x, y )
 end
 
 function scaleProp ( prop, scaleFactor, scaleFactorY )
-   local x, y = prop:getScl ()
-   if not scaleFactorY then
-      scaleFactorY = scaleFactor
-   end
-   prop:setScl ( x * scaleFactor, y * scaleFactorY )
+    local x, y = prop:getScl ()
+    if not scaleFactorY then
+        scaleFactorY = scaleFactor
+    end
+    prop:setScl ( x * scaleFactor, y * scaleFactorY )
 end
 
 function replaceProp ( layer, propId, frame, texturePack )
